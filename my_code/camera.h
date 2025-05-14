0e1e03eb4342e16a94ff453dd7b896a57b8052a4
yagih-at-live2d-com <yagi_hiroki@live2d.com>
2025-05-15 02:55:52 +0900

    second commit

--- my_code/camera.h
+++ my_code/camera.h
@@ -12,19 +12,22 @@ class camera {
         auto h = tan(theta / 2);
         auto viewport_height = 2.0 * h;
         auto viewport_width = aspect_ratio * viewport_height;
+
         w = unit_vector(lookfrom - lookat);
         u = unit_vector(cross(vup, w));
         v = cross(w, u);
+
         origin = lookfrom;
         horizontal = focus_dist * viewport_width * u;
         vertical = focus_dist * viewport_height * v;
         lower_left_corner =
             origin - horizontal / 2 - vertical / 2 - focus_dist * w;
-        lens_radius = aperture / 2;
     }
+
     ray get_ray(double s, double t) const {
         vec3 rd = lens_radius * random_in_unit_disk();
         vec3 offset = u * rd.x() + v * rd.y();
+
         return ray(origin + offset, lower_left_corner + s * horizontal +
                                         t * vertical - origin - offset);
     }
